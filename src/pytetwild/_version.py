dc1f98c2856f872adb88cf3c5eb845342902b072
Alex Kaszynski <akascap@gmail.com>
2024-03-01 22:50:52 -0700

    get version from metadata

--- src/pytetwild/_version.py
+++ src/pytetwild/_version.py
@@ -1,10 +1,4 @@
-"""Contains the pytetwild version.
+"""Contains the pytetwild version."""
+from importlib import metadata
 
-On the ``main`` branch, use 'dev0' to denote a development version.
-For example:
-
-version_info = 0, 27, 'dev0'
-
-"""
-version_info = 0, 1, "dev0"
-__version__ = ".".join(map(str, version_info))
+__version__ = metadata.version("pytetwild")
