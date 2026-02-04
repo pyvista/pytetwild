207d972ad1bb9a9cefff077597e6fad607765aa6
Marius Causemann <mariuscausemann@yahoo.de>
2026-02-04 13:30:28 +0100

    add disable_filtering parameter

--- src/pytetwild/pytetwild.py
+++ src/pytetwild/pytetwild.py
@@ -100,6 +100,7 @@ def tetrahedralize_pv(
     num_threads: int = 0,
     num_opt_iter: int = 80,
     loglevel: int = 3,
+    disable_filtering: bool = False,
     quiet: bool = True,
 ) -> "pv.UnstructuredGrid":
     """
@@ -138,7 +139,10 @@ def tetrahedralize_pv(
         Set log level (0 = most verbose, 6 = minimal output).
     quiet : bool, default: False
         Disable all output. Overrides ``loglevel``.
-
+    disable_filtering : bool, default: False
+        Disable the filtering of the resulting mesh, and thus keep
+        fTetWilds background mesh.
+        
     Returns
     -------
     pv.UnstructuredGrid
@@ -205,6 +209,7 @@ def tetrahedralize_pv(
         loglevel,
         quiet,
         vtk_ordering,
+        disable_filtering
     )
     return _ugrid_from_regular_cells(tmesh_v, tmesh_c)
 
@@ -224,6 +229,8 @@ def tetrahedralize(
     loglevel: int = 3,
     quiet: bool = True,
     vtk_ordering: bool = False,
+    disable_filtering: bool = False
+    
 ) -> tuple[NDArray[np.float64], NDArray[np.int32]]:
     """
     Convert mesh vertices and faces to a tetrahedral mesh.
@@ -264,7 +271,9 @@ def tetrahedralize(
         Disable all output. Overrides ``loglevel``.
     vtk_ordering : bool, default: False
         Reorder the tetrahedral cell indices to match VTK's ordering.
-
+    disable_filtering : bool, default: False
+        Disable the filtering of the resulting mesh, and thus keep 
+        fTetWilds background mesh.
     Returns
     -------
     Tuple[np.ndarray, np.ndarray]
@@ -301,6 +310,7 @@ def tetrahedralize(
         loglevel,
         quiet,
         vtk_ordering,
+        disable_filtering
     )
 
 
